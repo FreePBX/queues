a349fed5ee3574183107cd877be018b107b14ebe
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2013-11-11 20:21:11 +0000

    FREEPBX-6927 #Resolved #Comment remove space inserted in prepend creating prepepend stacking

--- functions.inc/dialplan.php
+++ functions.inc/dialplan.php
@@ -124,7 +124,7 @@ function queues_get_config($engine) {
 				// deal with group CID prefix
 				$ext->add($c, $exten, '', new ext_set('QCIDPP', '${IF($[${LEN(${VQ_CIDPP})}>0]?${VQ_CIDPP}' . ':' . ($grppre == '' ? ' ':$grppre) . ')}'));
 				$ext->add($c, $exten, '', new ext_set('VQ_CIDPP', ''));
-				$ext->add($c, $exten, '', new ext_execif('$["${QCIDPP}"!=""]', 'Macro', 'prepend-cid, ${QCIDPP}'));
+				$ext->add($c, $exten, '', new ext_execif('$["${QCIDPP}"!=""]', 'Macro', 'prepend-cid,${QCIDPP}'));
 
 				// Set Alert_Info
 				$ainfo = $alertinfo != '' ? str_replace(';', '\;', $alertinfo) : ' ';
