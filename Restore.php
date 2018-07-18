a56e164917aed72b8f4e0ebf82c7f8df9de9dbd4
James Finstrom <jfinstrom@sangoma.com>
2018-07-18 22:22:01 +0000

    Cleanup restore

--- Restore.php
+++ Restore.php
@@ -3,7 +3,7 @@ namespace FreePBX\modules\Queues;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
   public function runRestore($jobid){
-    $configs = $this->getConfigs();
+    $configs = reset($this->getConfigs());
     $this->FreePBX->Queues->loadConfigs($configs['configs'])
         ->loadDetails($configs['details']);
   }
